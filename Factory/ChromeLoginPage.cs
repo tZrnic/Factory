8dbcfe45dbc74eedf57a216afdbfef6beac87558
Zrnic <zrnic.tin123@gmail.com>
2024-05-19 22:58:08 +0200

    Add project files.

---
+++ Factory/ChromeLoginPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class ChromeLoginPage : LoginPage
+    {
+        public WebElement loginButton()
+        {
+            return new WebElement("Log in");
+        }
+        public WebElement usernameInput()
+        {
+            return new WebElement("user");
+        }
+        public WebElement passwordInput()
+        {
+            return new WebElement("lozinka");
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,20 @@
+﻿using Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program()
+{
+    public static void Main()
+    {
+        LoginPageFactory loginPageFactory = new ChromeLoginPageFactory();
+
+        LoginPage loginPage = loginPageFactory.CreatePage();
+
+        loginPage.passwordInput().Click();
+        loginPage.usernameInput().Click();
+        loginPage.loginButton().Click();
+    }
+}
